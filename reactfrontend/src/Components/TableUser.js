1b4caa27ca3854653404ba6588f6b49f83bdc3e1
Animasso Sidibe <asidibe017@gmail.com>
2023-01-05 19:31:47 +0100

    changement style table

--- reactfrontend/src/Components/TableUser.js
+++ reactfrontend/src/Components/TableUser.js
@@ -36,17 +36,36 @@ function TableUser(props) {
     previousPage,
     canNextPage,
     canPreviousPage,
+    pageOptions,
     prepareRow,
+    gotoPage,
+    pageCount,
+    setPageSize,
     state,
     setGlobalFilter,
   } = tableInstance;
-  const { globalFilter } = state;
+  const { globalFilter, pageIndex, pageSize } = state;
+
   return (
     <div className="h-screen flex flex-col bg-gradient-to-bl from-lime-100 via-lime-600 to-lime-900">
-      <div className=" mt-9 place-self-start ml-5">
+      <div className=" mt-9 flex flex-col flex-wrap ml-5">
         <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+        <select
+          className="mt-10 w-52 h-10 ml-11 "
+          name="pageTotal"
+          id="pageTotal"
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 15, 20].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize} Entries
+            </option>
+          ))}{" "}
+        </select>
       </div>
-      <div className=" overflow-x-auto ml-16">
+
+      <div className=" overflow-x-auto ml-4">
         <table
           id="customers"
           className=" table-auto mt-12 overflow-x-auto "
@@ -61,7 +80,7 @@ function TableUser(props) {
                     {...column.getHeaderProps(column.getSortByToggleProps())}
                   >
                     {column.render("Header")}
-                    <div className="ml-2 mt-1">
+                    <div className=" w-4 h-4">
                       {column.isSorted ? (
                         column.isSortedDesc ? (
                           <FaChevronDown />
@@ -84,7 +103,7 @@ function TableUser(props) {
             {page.map((row) => {
               prepareRow(row);
               return (
-                <tr {...row.getRowProps()}>
+                <tr className="hover:bg-slate-200" {...row.getRowProps()}>
                   {row.cells.map((cell) => {
                     return (
                       <td
@@ -101,7 +120,14 @@ function TableUser(props) {
           </tbody>
         </table>
       </div>
-      <div className=" inline-flex mt-4 ml-16 ">
+      <div className=" inline-flex mt-4 ml-4 ">
+        <button
+          className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
+          {"<<"}{" "}
+        </button>
         <button
           className="bnt-table bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
           onClick={() => previousPage()}
@@ -116,6 +142,40 @@ function TableUser(props) {
         >
           Next
         </button>
+        <button
+          className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
+          {">>"}{" "}
+        </button>
+      </div>
+
+      <div className=" mt-5 ml-8">
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+          {""}
+        </span>
+        <span>
+          {" "}
+          | Go to page{" "}
+          <input
+            type="number"
+            className=" w-12"
+            name="pageNum"
+            id="pageNum"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+          />
+        </span>
       </div>
       <div
         className="link-redirection text-center cursor-pointer font-mono font-bold text-2xl text-cyan-50 hover:text-slate-800 underline"
